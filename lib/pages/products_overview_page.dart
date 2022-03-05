05974e986205767642aac4c68d478319be27e6b1
Felipe Henrique <felipeh.soares3@gmail.com>
2022-03-05 11:06:09 -0300

    add products card

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
@@ -15,7 +16,9 @@ class ProductOverviewPage extends StatelessWidget {
       body: GridView.builder(
         padding: const EdgeInsets.all(10),
         itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+        itemBuilder: (ctx, i) => ProductItem(
+          product: loadedProducts[i],
+        ),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
