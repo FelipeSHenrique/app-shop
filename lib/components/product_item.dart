2f22348d44d5baef5f0641ec0015e81a3bd32d21
Felipe Henrique <felipeh.soares3@gmail.com>
2022-03-27 15:20:19 -0300

    remove product

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
@@ -29,7 +31,33 @@ class ProductItem extends StatelessWidget {
               icon: const Icon(Icons.edit),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (ctx) {
+                    return AlertDialog(
+                      title: Text('Apagar produto'),
+                      content: Text(
+                          'Tem certeza que deseja apagar o produto "${product.name}"?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(ctx).pop();
+                          },
+                          child: Text('Cancelar'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Provider.of<ProductList>(context, listen: false).removeProduct(product);
+                            Navigator.of(ctx).pop();
+                          },
+                          child: Text('Apagar'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
               color: Theme.of(context).errorColor,
               icon: const Icon(Icons.delete),
             ),
